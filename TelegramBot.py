67a79f8402723499d92686cf847c75d7fd5b1739
Filtux <96967851+Filtux@users.noreply.github.com>
2022-01-04 16:45:15 +1030

    Reverse?

--- TelegramBot.py
+++ TelegramBot.py
@@ -7,27 +7,9 @@ import QuizInitialTestLoad as tl
 import QuizBuzzerModifierCurrentFinal as qb
 import pyautogui
 import ScreenShotQuestionNo as ss
-from flask import Flask, request
 
 TOKEN = '5079172619:AAHCary8yVpgIOKcsxrJuQWv4h8ApxPhtww'
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://warm-plateau-09315.herokuapp.com' + TOKEN)
-    return "!", 200
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 @bot.message_handler('Greet')
 def greet(message):
@@ -212,6 +194,4 @@ def fastestFinger(message):
   qf.fastestFinger()
   qf.returnToGo()
 
-
-
-#bot.polling()
+bot.polling()
